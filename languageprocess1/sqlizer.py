3f24d714c23958bd8895a61ab320395c8b6df2d3
amitt001 <mail2amit19@gmail.com>
2014-10-28 23:36:19 +0530

    select and logging

--- languageprocess1/sqlizer.py
+++ languageprocess1/sqlizer.py
@@ -9,17 +9,20 @@ SQLIZE the Normal query data to sql syntax
 sqldict = {}
 
 sqldict.setdefault('SELECT',['choose','take','find', 'select', 'elect', 'pick'])
-sqldict.setdefault('CREATE',['make','build','create', 'design', 'form', 'setup', 'invent', 'establish', 'produce', 'start', 'initiate', 'generate', 'devise', 'concieve', 'new'])
+sqldict.setdefault('CREATE',['make','build','create', 'design', 'form', 'setup', 'invent', 'establish', 'produce', 'initiate', 'generate', 'devise', 'concieve', 'new'])
 sqldict.setdefault('INSERT',['insert','put','enter', 'include', 'set', 'fill', 'inject', 'in'])
 sqldict.setdefault('VALUES', ['value', 'data', 'values'])
 
 # specify more sql syntax
-#sqldict.setdefault()
-#sqldict.setdefault()
+sqldict.setdefault('*', ['all', 'each'])
+sqldict.setdefault('where', ['whose', 'where"s'])
 #sqldict.setdefault()
 #sqldict.setdefault()
 
 def sqlize(qinput):
+    # log queries
+    fobj = open('logs', 'a');
+    fobj.write('Query: ' + qinput + '\n')
 
     data = languageprocess1.stopword.stopwordremover(qinput)
     
@@ -30,6 +33,8 @@ def sqlize(qinput):
     
     data = ' '.join(data)
     data = sqltokenize(data)
+    fobj.write('SQLized: ' + data + '\n\n')
+    fobj.close()
     return data
 
 datatype = ['text', 'integer']
@@ -37,18 +42,28 @@ datatype = ['text', 'integer']
 def sqltokenize(qinput):
 
 #    data = qinput.replace('(','`').replace(')', '`').replace(',', '`').split('`')
-    data = qinput.split()
-    for index, value in enumerate(data):
-        if 'table' in value:
-            ind = data.index(value) 
-            data[ind+2] = '(' + data[ind+2] 
-        if 'integer' in value or 'text' in value:
-            data[index] = data[index] + ','
+    if "CREATE" in qinput:
+        data = qinput.split()
+        for index, value in enumerate(data):
+            if 'table' in value:
+                ind = data.index(value) 
+                data[ind+2] = '(' + data[ind+2] 
+            if 'integer' in value or 'text' in value:
+                data[index] = data[index] + ','
 
-    data = ' '.join(data)
-    data = data.rstrip(',') + ')'
+        data = ' '.join(data)
+        data = data.rstrip(',') + ')'
 
-    return data
+        return data
+
+    elif "SELECT" in qinput:
+        data = qinput.split()
+        for index, value in enumerate(data):
+            if 'table' in value:
+                data.pop(index)
+        data = ' '.join(data)
+        return data
+    return qinput
 
 
 if __name__ == '__main__':
